761d3be17b4c05088a706a5af79a3c145b4af6e2
[dyad] <git@dyad.sh>
2025-06-10 09:00:53 +0200

    Reverted all changes back to version 23bfc9eec45fa65f567ae657c242863aa237fd08

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-// ... (vorherige Imports bleiben gleich)
-import AdminDashboardLayout from './pages/Admin';
-import AdminDashboardPage from './pages/Admin/Dashboard';
-import AdminServices from './pages/Admin/Services';
-import AdminTestimonials from './pages/Admin/Bewertungen';
-import AdminGallery from './pages/Admin/Galerie';
-import AdminSettings from './pages/Admin/Einstellungen';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import UeberUns from "./pages/UeberUns";
+import Leistungen from "./pages/Leistungen";
+import Galerie from "./pages/Galerie";
+import Bewertungen from "./pages/Bewertungen";
+import Kontakt from "./pages/Kontakt";
+import Impressum from "./pages/Impressum";
+import Datenschutz from "./pages/Datenschutz";
+import Empfehlungsprogramm from "./pages/Empfehlungsprogramm";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/Admin";
+import AdminServices from "./pages/Admin/Services";
+import { SessionProvider } from "./components/SessionProvider";
+import ScrollToTop from "./components/ScrollToTop"; // Import the new ScrollToTop component
+
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -13,23 +28,27 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <SessionProvider>
-          <ScrollToTop />
+          <ScrollToTop /> {/* Add ScrollToTop here */}
           <Routes>
-            {/* ... (vorherige Routen bleiben gleich) */}
-            
-            {/* Admin Routes */}
-            <Route path="/admin" element={<AdminDashboardLayout />}>
-              <Route index element={<AdminDashboardPage />} />
-              <Route path="services" element={<AdminServices />} />
-              <Route path="bewertungen" element={<AdminTestimonials />} />
-              <Route path="galerie" element={<AdminGallery />} />
-              <Route path="einstellungen" element={<AdminSettings />} />
-            </Route>
-            
-            {/* ... (restliche Routen) */}
+            <Route path="/" element={<Index />} />
+            <Route path="/ueber-uns" element={<UeberUns />} />
+            <Route path="/leistungen" element={<Leistungen />} />
+            <Route path="/galerie" element={<Galerie />} />
+            <Route path="/bewertungen" element={<Bewertungen />} />
+            <Route path="/kontakt" element={<Kontakt />} />
+            <Route path="/impressum" element={<Impressum />} />
+            <Route path="/datenschutz" element={<Datenschutz />} />
+            <Route path="/empfehlungsprogramm" element={<Empfehlungsprogramm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/services" element={<AdminServices />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </SessionProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
+export default App;
