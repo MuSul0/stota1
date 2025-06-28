aff86af26899e52acba0e281d6bbdfd49f41b7e3
[dyad] <git@dyad.sh>
2025-06-28 23:25:42 +0200

    [dyad] wrote 4 file(s)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,20 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
-import { useSession } from './hooks/useSession'
-import AdminLayout from './pages/Admin/Layout'
-import Login from './pages/Login'
+import { useSession } from '@/hooks/useSession'
+import AdminLayout from '@/pages/Admin/Layout'
+import Login from '@/pages/Login'
+import NotFound from '@/pages/NotFound'
 
 export default function Router() {
   const { isLoading, session } = useSession()
 
-  if (isLoading) return <div>Lade...</div>
+  if (isLoading) return <div className="p-4">Lade...</div>
 
   return (
     <Routes>
       {!session ? (
-        <Route path="*" element={<Login />} />
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Login />} />
+        </>
       ) : (
         <Route path="/admin/*" element={<AdminLayout />} />
       )}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './globals.css'
 import { SessionProvider } from './context/SessionContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionProvider>
-      <App />
-    </SessionProvider>
+    <BrowserRouter>
+      <SessionProvider>
+        <App />
+      </SessionProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
