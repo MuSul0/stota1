c70a7e103ac18bbe530402c15fa65af3a4e191b8
[dyad] <git@dyad.sh>
2025-06-28 23:57:14 +0200

    [dyad] Fixed JSX wrapping error in App.tsx by properly nesting Route components inside Routes - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
-// ... bestehende Imports
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { Toaster } from 'sonner';
+import ScrollToTop from '@/components/ScrollToTop';
+import SessionProvider from '@/components/SessionProvider';
+import NotFound from '@/pages/NotFound';
+import Index from '@/pages/Index';
+import Leistungen from '@/pages/Leistungen';
+import UeberUns from '@/pages/UeberUns';
+import Galerie from '@/pages/Galerie';
+import Bewertungen from '@/pages/Bewertungen';
+import Kontakt from '@/pages/Kontakt';
+import Empfehlungsprogramm from '@/pages/Empfehlungsprogramm';
+import Impressum from '@/pages/Impressum';
+import Datenschutz from '@/pages/Datenschutz';
+import Login from '@/pages/Login';
 import KundenDashboard from '@/pages/kundenportal';
 import MitarbeiterDashboard from '@/pages/mitarbeiterportal';
 import AdminDashboard from '@/pages/adminportal';
 
-// ... in den Routes:
-<Route path="/kundenportal" element={<KundenDashboard />} />
-<Route path="/mitarbeiterportal" element={<MitarbeiterDashboard />} />
-<Route path="/adminportal" element={<AdminDashboard />} />
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster position="top-center" richColors expand={true} />
+        <BrowserRouter>
+          <SessionProvider>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/leistungen" element={<Leistungen />} />
+              <Route path="/ueber-uns" element={<UeberUns />} />
+              <Route path="/galerie" element={<Galerie />} />
+              <Route path="/bewertungen" element={<Bewertungen />} />
+              <Route path="/kontakt" element={<Kontakt />} />
+              <Route path="/empfehlungsprogramm" element={<Empfehlungsprogramm />} />
+              <Route path="/impressum" element={<Impressum />} />
+              <Route path="/datenschutz" element={<Datenschutz />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/kundenportal" element={<KundenDashboard />} />
+              <Route path="/mitarbeiterportal" element={<MitarbeiterDashboard />} />
+              <Route path="/adminportal" element={<AdminDashboard />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </SessionProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
